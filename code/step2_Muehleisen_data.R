c122527e2e650adb51a98ba1a0349571ed4e4cd1
Lauren Hallett <lauren.m.hallett@gmail.com>
2025-07-08 14:20:43 -0700

    update to calculate rel non and storage for all pairwise

--- code/step2_Muehleisen_data.R
+++ code/step2_Muehleisen_data.R
@@ -160,7 +160,11 @@ distout_all <- left_join(distout_relnon, distvar) %>%
   
 # ## Calculate for every pairwise combination of Muehleisen
   
-  pairwise_average <- data.frame(species1=character(), species2=character(), de=numeric(), extinct=numeric(), inferior = character())
+  pairwise_outcomes <- data.frame(species1=character(), species2=character(), de=numeric(), extinct=numeric(), inferior = character(), scenario = character(), prop = numeric())
+  
+  pairwise_calculations <- data.frame(species1=character(), species2=character(), de=numeric(), de_rellambda = numeric(), de_relalpha = numeric(),
+                                      de_storage = numeric(), rellambda = numeric(), relalpha = numeric(), storage = numeric(), superior = character(), swap_rellambda = numeric(),
+                                      superior_relnonlambda = numeric(), swap_relalpha = numeric(), superior_relnonalpha = numeric(), swap_storage = numeric())
   # create vector of species
   splist <- unique(d2vr$species)
   
@@ -170,6 +174,8 @@ distout_all <- left_join(distout_relnon, distvar) %>%
   for (i in 1:ncol(spcombos)) {
     #subset to focal species pairs
     spcomboi <- spcombos[,i]
+    
+    ### mean conditions - create vector and matrix ###
     vrsubmean <- subset(vrmean, species%in%c(spcomboi))
     intsubmean <- subset(intmean, species%in%c(spcomboi)) %>%
       pivot_longer(names_to = "competitor", values_to ="value", AVFA_sum:VUMY_sum) %>%
@@ -178,27 +184,108 @@ distout_all <- left_join(distout_relnon, distvar) %>%
       filter(competitor%in%c(spcombos[,i])) %>%
       pivot_wider(names_from = competitor, values_from = value)
     
-    
     # create growth rate vector and interaction matrix
-    r <- vrsubmean$nu_weightedmean
-    A <- as.matrix(intsubmean[,2:3])
+    r_mean <- vrsubmean$nu_weightedmean
+    A_mean <- as.matrix(intsubmean[,2:3])
     
     #we multiply by -1 as competition should be negative
-    A <- A*-1
+    A_mean <- A_mean*-1
     
-    de <- calculate_distance_to_border_2sp(A, r)
+    ### dry conditions - create vector and matrix ###
+    vrsubdry <- subset(vrdry, species%in%c(spcomboi))
+    intsubdry <- subset(intdry, species%in%c(spcomboi)) %>%
+      pivot_longer(names_to = "competitor", values_to ="value", AVFA:VUMY) %>%
+      filter(competitor%in%c(spcombos[,i])) %>%
+      pivot_wider(names_from = competitor, values_from = value)
     
-    tempout <- data.frame(species1= spcomboi[1], species2 = spcomboi[2], de = de[[1]][1], extinct = de[[2]][1], superior = de[[3]][1])
-    pairwise_average <- rbind( pairwise_average, tempout)
+    r_dry <- vrsubdry$nu
+    A_dry <- as.matrix(intsubdry[,2:3])*-1
+      
+
+    ### wet conditions - create vector and matrix ###
+    vrsubwet <- subset(vrwet, species%in%c(spcomboi))
+    intsubwet <- subset(intwet, species%in%c(spcomboi)) %>%
+      pivot_longer(names_to = "competitor", values_to ="value", AVFA:VUMY) %>%
+      filter(competitor%in%c(spcombos[,i])) %>%
+      pivot_wider(names_from = competitor, values_from = value)
+    
+    r_wet <- vrsubwet$nu
+    A_wet <- as.matrix(intsubwet[,2:3])*-1
+
+
+    ## coexistence in average and static conditions
+    de_mean <- calculate_distance_to_border_2sp(A_mean, r_mean)
+    de_dry <- calculate_distance_to_border_2sp(A_dry, r_dry)
+    de_wet <- calculate_distance_to_border_2sp(A_wet, r_wet)
+    
+    ## relative nonlinearity in lambdas
+    de_wr_ai <- calculate_distance_to_border_2sp(A_mean, r_wet)
+    de_dr_ai <- calculate_distance_to_border_2sp(A_mean, r_dry)
+    
+    ## relative nonlinearity in alphas
+    de_ar_wi <- calculate_distance_to_border_2sp(A_wet, r_mean)
+    de_ar_di <- calculate_distance_to_border_2sp(A_dry, r_mean)
     
     
-    #draw the cone
-    draw_biodiversity_cone(A, spcomboi[1], spcomboi[2])
+    
+
+    de_mean_out <- data.frame(species1= spcomboi[1], species2 = spcomboi[2], de = de_mean[[1]][1], extinct = de_mean[[2]][1], superior = de_mean[[3]][1], scenario = "average", prop = 1)
+   
+    de_dry_out <- data.frame(species1= spcomboi[1], species2 = spcomboi[2], de = de_dry[[1]][1], extinct = de_dry[[2]][1], superior = de_dry[[3]][1], scenario = "dry", prop = pd)
+    de_wet_out <- data.frame(species1= spcomboi[1], species2 = spcomboi[2], de = de_wet[[1]][1], extinct = de_wet[[2]][1], superior = de_wet[[3]][1], scenario = "wet", prop = pw)
+   
+    sharedwinner_storage <- ifelse(identical(de_dry_out$superior, de_wet_out$superior) == T, 1, 0)
+     storage_out <- rbind(de_dry_out, de_wet_out) %>%
+       mutate(de_weighted = de*prop) %>%
+       mutate(de_storage = ifelse(identical(de_dry_out$superior, de_wet_out$superior) & sum(de_dry_out$extinct, de_wet_out$extinct) == 2, sum(de_weighted), sum(abs(de_weighted))),
+              swap_storage = ifelse(!identical(de_dry_out$superior, de_wet_out$superior) & (sum(de_dry_out$extinct, de_wet_out$extinct) == 2), 1, 0)) %>%
+       mutate(superior_storage = superior) %>%
+       select(-(de:de_weighted))
+       
+    de_wr_ai_out <- data.frame(species1= spcomboi[1], species2 = spcomboi[2], de = de_wr_ai[[1]][1], extinct = de_wr_ai[[2]][1], superior = de_wr_ai[[3]][1], scenario = "wr_ai", prop = pw)
+    de_dr_ai_out <- data.frame(species1= spcomboi[1], species2 = spcomboi[2], de = de_dr_ai[[1]][1], extinct = de_dr_ai[[2]][1], superior = de_dr_ai[[3]][1], scenario = "dr_ai", prop = pd)
+    relnonlambda_out <- rbind(de_wr_ai_out, de_dr_ai_out) %>%
+      mutate(de_weighted = de*prop) %>%
+      mutate(de_rellambda = ifelse(identical(de_wr_ai_out$superior, de_dr_ai_out$superior) & sum(de_wr_ai_out$extinct, de_dr_ai_out$extinct) == 2, sum(de_weighted), sum(abs(de_weighted))),
+             swap_rellambda = ifelse(!identical(de_wr_ai_out$superior, de_dr_ai_out$superior) & (sum(de_wr_ai_out$extinct, de_dr_ai_out$extinct) == 2), 1, 0)) %>%
+      mutate(superior_relnonlambda = superior) %>%
+      select(-(de:de_weighted))
+    
+    de_ar_wi_out <- data.frame(species1= spcomboi[1], species2 = spcomboi[2], de = de_ar_wi[[1]][1], extinct = de_ar_wi[[2]][1], superior = de_ar_wi[[3]][1], scenario = "ar_wi", prop = pw)
+    de_ar_di_out <- data.frame(species1= spcomboi[1], species2 = spcomboi[2], de = de_ar_di[[1]][1], extinct = de_ar_di[[2]][1], superior = de_ar_di[[3]][1], scenario = "ar_di", prop = pd)
+    relnonalpha_out <- rbind(de_ar_wi_out, de_ar_di_out) %>%
+      mutate(de_weighted = de*prop) %>%
+      mutate(de_relalpha = ifelse(identical(de_ar_wi_out$superior, de_ar_di_out$superior) & sum(de_ar_wi_out$extinct, de_ar_di_out$extinct) == 2, sum(de_weighted), sum(abs(de_weighted))),
+             swap_relalpha = ifelse(!identical(de_ar_wi_out$superior, de_ar_di_out$superior) & (sum(de_ar_wi_out$extinct, de_ar_di_out$extinct) == 2), 1, 0)) %>%
+      mutate(superior_relnonalpha = superior) %>%
+      select(-(de:de_weighted))
+    
+    
+    
+    calculations_temp <- full_join(full_join(de_mean_out %>% select(-extinct, -scenario, -prop), full_join(storage_out[1,], relnonlambda_out[1,])), relnonalpha_out[1,]) %>%
+      mutate(rellambda = de_rellambda - de, relalpha = de_relalpha - de, storage = de_storage - de) %>%
+      select(species1, species2, de, de_rellambda, de_relalpha, de_storage, rellambda, relalpha, storage,  swap_rellambda, superior_relnonlambda, swap_relalpha, superior, superior_relnonalpha, swap_storage, superior_storage) 
+    
+    pairwise_calculations <- rbind(pairwise_calculations, calculations_temp)
+    pairwise_outcomes <- rbind( pairwise_outcomes, de_mean_out, de_dry_out, de_wet_out, de_wr_ai_out, de_dr_ai_out, de_ar_wi_out, de_ar_di_out)
+    
+  }
+    
+    
+    #draw the cone - mean
+    draw_biodiversity_cone(A_mean, spcomboi[1], spcomboi[2])
     #draw the vector
-    point <-r/sqrt(sum(r^2))
+    point <-r_mean/sqrt(sum(r_mean^2))
     arrows(0, 0, x1 = point[1], y1 = point[2], length = 0.15, angle = 30,
            code = 2, col = "brown", lty =1, lwd=2)
     text(0.8, 1, "A. Average conditions", cex = 1.2, pos = 1)
+    
+    
+    
+    
+    
+    
+    
     
     
   }
